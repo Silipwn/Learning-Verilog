d5648112dc4f99f6278dee2b8565090fe6a29316
Ashwin Nambiar <ashwin0nambiar@gmail.com>
2018-01-15 13:48:06 +0530

    Sorted #1 Created a New Folder for Unfinished Code

--- Multiplexer_2018-01-15_Stimulus.v
+++ Multiplexer_2018-01-15_Stimulus.v
@@ -9,26 +9,26 @@ wire OUTPUT;
 Multi mux(OUTPUT,IN0,IN1,IN2,IN3,S1,S0);
 
 //Begin Stimulation
-intial 
+initial 
     begin
     //setting INPUT
     IN0=1 ; IN1=0; IN2= 1; IN3=0;
     #1 $display("IN0=%b,IN1=%b,IN2=%B,IN3=%b\n",IN0,IN1,IN2,IN3);
     
     //IN0
-    S1=0; S0=0
+    S1=0; S0=0;
     #1 $display("S1 = %b, S0 = %b, O/P = %b \n",S1,S0,OUTPUT);
     
     //IN1
-    S1=0; S0=1
+    S1=0; S0=1;
     #1 $display("S1 = %b, S0 = %b, O/P = %b \n",S1,S0,OUTPUT);
     
     //IN2
-    S1=1; S0=0
+    S1=1; S0=0;
     #1 $display("S1 = %b, S0 = %b, O/P = %b \n",S1,S0,OUTPUT);
     
     //IN3
-    S1=1; S0=1
+    S1=1; S0=1;
     #1 $display("S1 = %b, S0 = %b, O/P = %b \n",S1,S0,OUTPUT);
     end 
 endmodule
--- conditional.v
+++
@@ -1,6 +0,0 @@
-if (enable== 1'b1) begin
-data= 10;
-address =16'hDEAD;
-wr_enable=1'b1;
-end else begin
-data =32'b0;
