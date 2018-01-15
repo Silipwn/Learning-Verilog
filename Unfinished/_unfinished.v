f5b362f71cd64635d58feca574274d75b89a77fe
Ashwin Nambiar <ashwin0nambiar@gmail.com>
2018-01-15 13:51:31 +0530

    Adding Unfinished Folder

---
+++ Unfinished/_unfinished.v
@@ -0,0 +1,15 @@
+module adder(a,b,c,s);
+    input a,b;
+    output c,s;
+    
+    assign c = a&b;
+    assign s = a^b;
+endmodule
+
+module sub(a,b,c,s);
+    input(
+
+module display(s0,s1,a,b,c,d,e,f,g);
+    input s0,s1;
+    output a,b,c,d,e,f,g;
+    
